c72348df9dac47403d406955d6218802d189ab2e
temadripeace <71979854+temadripeace@users.noreply.github.com>
2025-08-11 14:15:44 +0300

    GeoRound.py

--- GeoRound.py
+++ GeoRound.py
@@ -10,7 +10,7 @@ from io import BytesIO
 
 col1, col2, col3= st.columns([1, 3, 1])  # Left, Center, Right columns
 with col2:
-    st.image("Sucafina Logo.jpg", width=200)
+    st.image("Sucafina Logo.jpg", width=500)
 
 st.markdown("<h3 style='text-align: center;'>Geographic Coordinate Formarting Tool - 6DP</h3>", unsafe_allow_html=True)
 
@@ -218,4 +218,5 @@ if uploaded_file:
 
     except Exception as e:
         st.error(f"Error loading file: {e}")
+
 
