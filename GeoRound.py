1d8b54c9f3afc3f98fd608d4c399b487578c8559
temadripeace <71979854+temadripeace@users.noreply.github.com>
2025-09-26 08:28:24 +0300

    GeoRound.py

--- GeoRound.py
+++ GeoRound.py
@@ -5,6 +5,8 @@ import geopandas as gpd
 from shapely import wkt
 from shapely.geometry import Point, Polygon, MultiPolygon, MultiPoint
 from io import BytesIO
+import subprocess
+from osgeo import gdal
 
 col1, col2, col3 = st.columns([1, 3, 1])  # Left, Center, Right columns
 with col2:
@@ -238,6 +240,7 @@ if uploaded_file:
 
     except Exception as e:
         st.error(f"Error loading file: {e}")
+
 
 
 
